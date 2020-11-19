0f5c53ef3884dabb9e01bf069ae4266798a28e3f
Gokul Kolady <gokulk@mit.edu>
2020-11-19 15:31:32 -0500

    top_level with lowpass added

--- vivado/src/hdl/top_level.sv
+++ vivado/src/hdl/top_level.sv
@@ -64,9 +64,9 @@ module top_level(
 	logic pwm_val; //pwm signal (HI/LO)
 	
 	always_comb begin
-	   speaker_mid = sw[0]?(speaker_out <<< 6): 0;
+	   //speaker_mid = sw[0]?(speaker_out <<< 6): 0;
 	   speaker_out_switched = 0;
-	   //speaker_out_switched = speaker_mid[15:8];
+	   //speaker_out_switched = speaker_out[15:8];
 	end
 	
 	volume_control vc (.vol_in(sw[15:13]),
@@ -74,16 +74,26 @@ module top_level(
     pwm (.clk_in(clk_100mhz), .rst_in(btnd), .level_in({~vol_out[7],vol_out[6:0]}), .pwm_out(pwm_val));
     assign aud_pwm = pwm_val?1'bZ:1'b0;
     
+    logic lowpass_done; //pulse when lowpass is done computing
+    logic signed [15:0] lowpass_out; //output of lowpass filter
+    //initialize lowpass instance
+    lowpass lp_filter(.clk_in(clk_100mhz),
+                      .rst_in(btnd),
+                      .ready_in(sample_pulse),
+                      .done_out(lowpass_done),
+                      .signal_in(test_sample_right),
+                      .signal_out(lowpass_out));
+    
     //initialize sample buffer instance
     sampler sampler_buffer(.clk_in(clk_100mhz),
                            .rst_in(btnd),
-                           .ready_in(sample_pulse),
-                           .signal_in(1775+test_sample_right),
+                           .ready_in(lowpass_done),
+                           .signal_in(1775+lowpass_out),
                            .sample_out(sample),
                            .offset(offset));
     
     //initialize error calculator instance
-    error_calculator find_error(.feedback_in(1775+test_sample_right+speaker_out),//[25:10]),
+    error_calculator find_error(.feedback_in(1775+lowpass_out+speaker_out),//[25:10]),
                                 .error_out(error),
                                 .nc_on(sw[0]),
                                 .clk_in(clk_100mhz));
@@ -91,7 +101,7 @@ module top_level(
     //initialize LMS instance
     LMS lms1(.clk_in(clk_100mhz), 
              .rst_in(btnd),
-             .ready_in(sample_pulse),
+             .ready_in(lowpass_done),
              .error_in(error),
              .sample_in(sample),
              .offset_in(offset),
@@ -114,7 +124,8 @@ module top_level(
 		.probe1(test_sample_right),
 		.probe2(speaker_out_switched),
 		.probe3(sample_pulse),
-		.probe4(speaker_out)
+		.probe4(speaker_out),
+		.probe5(lowpass_out)
 	);
     
 endmodule
